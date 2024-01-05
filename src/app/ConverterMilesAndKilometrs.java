91ca9d0cedc45c472d03b4acb413f8d705e1f7c3
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-05 18:09:05 +0200

    add method main

--- src/app/ConverterMilesAndKilometrs.java
+++ src/app/ConverterMilesAndKilometrs.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class ConverterMilesAndKilometrs {
     private static final double COEFFICIENT_MILES_CONSTANT = 1.60934;
-    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть число миль через кому, яке потрібно конвертувати в кілометри");
+        double milesToKilometrs = scanner.nextDouble();
+        double kilometrs = convertMilesToKilometers(milesToKilometrs);
+        printResult(milesToKilometrs,"міль", kilometrs,"кілометр" );
+
+        System.out.println("Введіть число кілометрів через кому, яке потрібно конвертувати в мілі");
+        double kilometrsToMiles = scanner.nextDouble();
+        double miles = convertKilometrsToMiles(kilometrsToMiles);
+        printResult(kilometrsToMiles, "кілометр", miles, "міль");
+
+    }
 
     public static double convertMilesToKilometers(double miles) {
         double kilometers = miles * COEFFICIENT_MILES_CONSTANT;
@@ -17,7 +31,7 @@ public class ConverterMilesAndKilometrs {
     }
 
     public static void printResult (double inputData, String fromInput, double outputData, String fromOutput){
-        System.out.println(inputData + " " +fromInput + " " + outputData + " " + fromOutput );
+        System.out.println(inputData + " " +fromInput + " = " + outputData + " " + fromOutput );
     }
 
 }
